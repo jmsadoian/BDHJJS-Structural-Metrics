c6bf39052cc7ae3a9984136899ea8e91b0ac98e7
Daniel Koberstein <daniel.koberstein@wsu.edu>
2018-09-24 15:01:26 -0700

    number of var declarations partially working (no nested declarations can be found)

--- net.sf.eclipsecs.deliverable1/src/net/sf/eclipsecs/sample/checks/NumberOfComments.java
+++ net.sf.eclipsecs.deliverable1/src/net/sf/eclipsecs/sample/checks/NumberOfComments.java
@@ -1,42 +1,48 @@
 package net.sf.eclipsecs.sample.checks;
 
+import java.io.Console;
+
 import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.RootModule;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
+//not complete 
 public class NumberOfComments extends AbstractCheck {
 
 
   @Override
   public int[] getAcceptableTokens() {
-    return new int[] { TokenTypes.EOF };
+    System.out.println("in getAcceptableTokens");
+    return getRequiredTokens();
   }
 
   @Override
   public int[] getRequiredTokens() {
-	  return new int[0];
+    System.out.println("in getRequiredTokens");
+    return new int[0];
   }
 
   @Override
   public int[] getDefaultTokens() {
-	  return new int[] { TokenTypes.EOF };
+    System.out.println("in getDefaultTokens");
+    return getRequiredTokens();
   }
 
   @Override
   public void visitToken(DetailAST ast) {
+    System.out.println("in visitToken");
     int count = 0;
     
     //DetailAST root = ast.getParent();
     // find the OBJBLOCK node below the CLASS_DEF/INTERFACE_DEF
     count += ast.getChildCount(TokenTypes.SINGLE_LINE_COMMENT);
-    count += ast.getChildCount(TokenTypes.BLOCK_COMMENT_BEGIN);
 
     //DetailAST objBlock = ast.findFirstToken(TokenTypes.OBJBLOCK);
     // count the number of direct children of the OBJBLOCK
     
     //int classComments = objBlock.getChildCount(TokenTypes.SINGLE_LINE_COMMENT);
-    
+    System.out.println(count);
     log(ast.getLineNo(), "numberofcomments", count);
     
   }
